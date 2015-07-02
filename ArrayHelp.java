5482ce647e2ea8e05a42740d44c297d2fd1d5d41
Rus1k <rfrasulov@gmail.com>
2015-07-02 04:39:03 +0300

    lessons3

---
+++ ArrayHelp.java
@@ -0,0 +1,30 @@
+package arrays;
+
+import java.lang.reflect.Array;
+import java.util.*;
+import java.util.Arrays;
+
+import static java.util.Arrays.*;
+
+/**
+ * Created by ruslan on 30.06.2015.
+ */
+public class ArrayHelp {
+
+    public int[] leftUnion(int[] leftArray, int[] rightArray) {
+        int[] result = new int[leftArray.length + rightArray.length];
+        result = Arrays.copyOf(leftArray, result.length);
+        int sumInt = 0;
+        for (int i = 0; i < rightArray.length; i++) {
+            for (int j = 0; j < leftArray.length; j++) {
+                if (rightArray[i] == leftArray[j]) {
+                    result[leftArray.length + (sumInt++)] = rightArray[i];
+                }
+            }
+        }
+        result = Arrays.copyOfRange(result, 0, leftArray.length + sumInt);
+        return result;
+
+    }
+
+}
---
+++ Arrays.java
@@ -0,0 +1,20 @@
+package arrays;
+
+/**
+ * Created by ruslan on 30.06.2015.
+ */
+public class Arrays {
+    public static void main(String[] args) {
+        int[] mas1 = {1, 5, 4, 23, 65, 32, 78};
+        int[] mas2 = {3, 5, 24, 4, 1, 2, 34, 45, 32, 5};
+
+        ArrayHelp ah = new ArrayHelp();
+
+        int[] mas3 = ah.leftUnion(mas1, mas2);  // Use case for left union
+
+        for (int i = 0; i < mas3.length; i++) {
+            System.out.print(mas3[i] + " ");
+        }
+
+    }
+}
